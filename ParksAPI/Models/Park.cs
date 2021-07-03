5c10bbd7b1716d60bf9e6d2fb229bfdf340af4b5
Karlson Drendel <kdrendel99@gmail.com>
2021-07-02 17:06:19 -0700

    Build project environment - Initial commit

---
+++ ParksAPI/Models/Park.cs
@@ -0,0 +1,10 @@
+namespace ParksAPI.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string State { get; set; }
+    public string MainAttraction { get; set; }
+  }
+}
---
+++ ParksAPI/Models/ParksAPIContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksAPI.Models
+{
+  public class ParksAPIContext : DbContext
+  {
+    public ParksAPIContext(DbContextOptions<ParksAPIContext> options) : base(options)
+    {
+    }
+    public DbSet<Park> Parks { get; set; }
+  }
+}
