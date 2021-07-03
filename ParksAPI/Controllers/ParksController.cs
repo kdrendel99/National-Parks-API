c6b3fd64c5046de0709a6b7619217438666b4eda
Karlson Drendel <kdrendel99@gmail.com>
2021-07-02 17:10:08 -0700

    Create GET and POST routes for ParksController

---
+++ ParksAPI/Controllers/ParksController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using ParksAPI.Models;
+
+namespace ParksAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private readonly ParksAPIContext _db;
+
+    public ParksController(ParksAPIContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/Parks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get()
+    {
+      return await _db.Parks.ToListAsync();
+    }
+
+    // POST api/Parks
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
+    }
+  }
+}
